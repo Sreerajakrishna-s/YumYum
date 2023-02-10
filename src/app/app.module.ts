be652b3d3ea9ffdf37dfc64d446877e091332542
sree raja krishna <sreerajakrishna2002@gmail.com>
2023-02-10 11:34:03 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BookComponent } from './book/book.component';
 import { CartComponent } from './cart/cart.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SearchComponent } from './search/search.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 const routes: Routes = [
   {
     path:'', component:HomeComponent,pathMatch:'full'
   }, 
   {
     path:'about', component:AboutComponent
-  }, 
-  {
-    path:'book', component:BookComponent
-
-  }, 
+  },  
   {
     path:'cart', component:CartComponent
   },
  
   {
     path:'menu', component:MenuComponent
+  }, 
+  {
+    path:'contact', component:ContactComponent
   }, 
 
 
@@ -38,17 +38,18 @@ const routes: Routes = [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    BookComponent,
     CartComponent,
     ProfileComponent,
     SearchComponent,
     AboutComponent,
-    MenuComponent
+    MenuComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
+    ReactiveFormsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
